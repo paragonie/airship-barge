5a8d1bc66c4b966bb03a6e1e282f65197f8d47fb
Scott <scott@paragonie.com>
2015-10-14 00:10:21 -0400

    Add signing to build process.

--- src/Commands/Build.php
+++ src/Commands/Build.php
@@ -1,7 +1,10 @@
 <?php
 namespace Airship\Barge\Commands;
 
-use Airship\Barge as Base;
+use \Airship\Barge as Base;
+use \ParagonIE\Halite\File;
+use ParagonIE\Halite\Asymmetric\Crypto as Asymmetric;
+use ParagonIE\Halite\Key;
 
 class Build extends Base\Command
 {
@@ -63,13 +66,69 @@ class Build extends Base\Command
             $phar->createDefaultStub('src/autoload.php', 'src/autoload.php')
         );
         $phar->setMetadata($manifest);
-        
-        // Step Two -- Do we have the signing key?
         if (isset($this->config['vendors'][$manifest['vendor']])) {
-            $vendor =& $this->config['vendors'][$manifest['vendor']];
-            foreach ($vendor['signing_keys'] as $sign_key) {
-                /** @todo 2015-10-10 - make key management, then come back */
-            }
+            $this->sign(
+                $manifest['vendor'],
+                $path,
+                $pharname
+            );
         }
+    }
+    
+    protected function sign($vendor_name, $path, $pharname)
+    {
+        $TAB = str_repeat(' ', self::TAB_SIZE);
+        $HTAB = str_repeat(' ', ceil(self::TAB_SIZE / 2));
+        
+        $vendor =& $this->config['vendors'][$vendor_name];
+        $numKeys = \count($vendor['signing_keys']);
+        if ($numKeys > 1) {
+            echo 'You have more than one signing key available.';
+
+            $n = 1;
+            $size = (int) \floor(\log($numKeys, 10));
+            $key_associations = $HTAB."ID\tPublic Key\n";
+            foreach ($vendor['signing_keys'] as $sign_key) {
+                $_n = \str_pad($n, $size, ' ', STR_PAD_LEFT);
+                $key_associations .= $HTAB.$_n.$HTAB.$sign_key['public_key']."\n";
+                ++$n;
+            }
+            // Let's ascertain the user's key selection
+            do {
+                $choice = (int) $this->prompt('Enter the ID for the key you wish to use: ');
+                if ($choice < 1 || $choice > $numKeys) {
+                    $choice = null;
+                }
+            } while(empty($choice));
+            $skey = $vendor['signing_keys'][$choice - 1];
+        } else {
+            $skey = $vendor['signing_keys'][0];
+        }
+        
+        if (empty($skey['salt'])) {
+            echo 'Salt not found for this key.', "\n";
+            exit(255);
+        }
+        
+        $password = $this->silentPrompt('Enter Password:');
+        
+        $salt = \Sodium\hex2bin($skey['salt']);
+        list($sign_secret, $sign_public) = Key::deriveFromPassword(
+            $password,
+            $salt,
+            Key::CRYPTO_SIGN
+        );
+        $pubkey = \Sodium\bin2hex($sign_public->get());
+        if ($skey['public_key'] !== $pubkey) {
+            echo 'Invalid password', "\n";
+            exit(255);
+        }
+        $checksum = File::checksumFile($path.'/dist/'.$pharname, null, true);
+        $signature = Asymmetric::sign($checksum, $sign_secret);
+        
+        return \file_put_contents(
+            $path.'/dist/'.$pharname.'.ed25519.sig',
+            $signature
+        );
     }
 }
